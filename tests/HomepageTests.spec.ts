dc7127fc81f9acc2e1ce3f880329df5fc62fafa2
Stuart Dougans <stuart.dougans@gmail.com>
2024-02-20 00:36:20 +0000

    using toBeInViewport instead of isVisible

--- tests/HomepageTests.spec.ts
+++ tests/HomepageTests.spec.ts
@@ -30,39 +30,39 @@ test.describe('Landing experience is nudger or forced audience selector for new 
     
     test('should be nudger for /en/uk/individual-investors/', async () => {
         await page.goto(base_url + "/en/uk/individual-investors/");
-        expect(await locator_nudger).toBeVisible;
+        expect(await locator_nudger).toBeInViewport();
         const nudger_text = await locator_nudger.textContent();
         expect(await nudger_text).toContain("You are viewing this site as an Individual investor in UK.");
     });
 
     test('should be nudger for /en/uk/institutional-investor/', async () => {
         await page.goto(base_url + "/en/uk/institutional-investor/");
-        expect(await locator_nudger).toBeVisible;
+        expect(await locator_nudger).toBeInViewport();
         const nudger_text = await locator_nudger.textContent();
         expect(await nudger_text).toContain("You are viewing this site as an Institutional investor in UK.");
     });
 
     test('should be nudger for /en/uk/intermediaries/', async () => {
         await page.goto(base_url + "/en/uk/intermediaries/");
-        expect(await locator_nudger).toBeVisible;
+        expect(await locator_nudger).toBeInViewport();
         const nudger_text = await locator_nudger.textContent();
         expect(await nudger_text).toContain("You are viewing this site as an Intermediaries in UK.");
     });
 
     test('should be Forced Audience Selector for USA channel', async () => {
         await page.goto(base_url + "/en/usa/professional-investor/");
-        expect(await locator_audienceSelector).toBeVisible;
-        expect(await locator_audienceSelector.getAttribute("aria-hidden")).toBeFalsy;
-        expect(await locator_audienceSelectorRegion).toBeVisible;
+        expect(await locator_audienceSelector).toBeInViewport();
+        expect(await locator_audienceSelector.getAttribute("aria-hidden")).toBe("false");
+        expect(await locator_audienceSelectorRegion).toBeInViewport();
         expect(await locator_audienceSelectorRegion.textContent()).toContain("01. Your location");
         expect(await locator_currentCountry.textContent()).toContain("USA");
     });
 
     test('should be Forced Audience Selector for Global channel', async () => {
         await page.goto(base_url + "/en/global/all-users/");
-        expect(await locator_audienceSelector).toBeVisible;
-        expect(await locator_audienceSelector.getAttribute("aria-hidden")).toBeFalsy;
-        expect(await locator_audienceSelectorRegion).toBeVisible;
+        expect(await locator_audienceSelector).toBeInViewport();
+        expect(await locator_audienceSelector.getAttribute("aria-hidden")).toBe("false");
+        expect(await locator_audienceSelectorRegion).toBeInViewport();
         expect(await locator_audienceSelectorRegion.textContent()).toContain("01. Your location");
         expect(await locator_currentCountry.textContent()).toContain("Global");
     });
@@ -72,7 +72,7 @@ test.describe('Landing experience is nudger or forced audience selector for new 
 test.describe('Suppressed landing experience for returning users', () => {
 
     test('should be no nudger for /en/uk/individual-investors/', async () => {
-        
+
     });
 
     test('should be no nudger for /en/uk/institutional-investor/', async () => {
