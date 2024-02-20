8f2eaf8c00cfd28b8ae61617b2766c668cf1bc75
Stuart Dougans <stuart.dougans@gmail.com>
2024-02-20 22:59:37 +0000

    add html report to config for CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 3 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? [['junit', { outputFile: 'test-results/e2e-junit-results.xml' }]] : [ ['html', { open: 'never' }] ],
+  reporter: process.env.CI ? [['junit', { outputFile: 'junit-test-results/e2e-junit-results.xml' }], ['html', {open:'never', outputFile: 'html-test-results/html-report'}]] : [['html', {open:'never'}]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
