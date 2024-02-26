5bacef95a26298705d889aec0cbea1262d2079a9
Stuart Dougans <stuart.dougans@gmail.com>
2024-02-26 23:19:21 +0000

    add tests for strategy landing page

--- page-objects/strategies-pageObject.ts
+++ page-objects/strategies-pageObject.ts
@@ -3,8 +3,55 @@ import { expect, type Locator, type Page } from '@playwright/test';
 export class StrategyPage {
   readonly page: Page;
 
+  readonly pageHeading1: Locator;
+  readonly searchBar: Locator;
+  readonly searchBar_searchButton: Locator;
+  readonly searchFilters_title: Locator;
+  readonly searchFilters_list: Locator;
+  readonly searchFilters_listButtons: Locator;
+  readonly strategyCards: Locator;
+
   constructor(page: Page) {
     this.page = page;
+
+    this.pageHeading1 = page.locator('h1');
+    this.searchBar = page.locator('[class^="PageSearchBar_page-search-bar-container"]');
+    this.searchBar_searchButton = page.locator('.search-button');
+    this.searchFilters_title = page.locator('h3[class^="SearchDropdownFilters_desktopBarHeading"]');
+    this.searchFilters_list = page.locator('ul[class^="SearchDropdownFilters_desktopBarMenu"]');
+    this.searchFilters_listButtons = page.locator('button[class^="LinkButtonStyles_dropdownButton"]');
+    this.strategyCards = page.locator('#intersection-search-entry [class^="CardGridListing_gridResultsItem"]');
+
+  }
+
+  async pageTitleIsCorrect(title: string){
+    expect(await this.pageHeading1.first().textContent()).toContain(title);
+  }
+
+  async searchBarIsInViewport(){
+    await expect(this.searchBar).toBeInViewport();
+  }
+
+  async searchBarSearchButtonIsInViewport(){
+    await expect(this.searchBar_searchButton).toBeInViewport();
+  }
+
+  async searchFiltersTitleIsInViewport(){
+    await expect(this.searchFilters_title).toBeInViewport();
+  }
+
+  async searchFiltersListIsInViewport(){
+    await expect(this.searchFilters_list).toBeInViewport();
+  }
+
+  async multipleSearchFiltersButtonsExist(){
+    let numberOfFilterButtons = (await this.searchFilters_listButtons.all()).length;
+    expect(numberOfFilterButtons).toBeGreaterThan(0);
+  }
+
+  async multipleStrategyCardsExist(){
+    let numberOfStrategyCards = (await this.strategyCards.all()).length;
+    expect(numberOfStrategyCards).toBe(15);
   }
 
 }
--- tests/StrategyTests.spec.ts
+++ tests/StrategyTests.spec.ts
@@ -1,11 +1,13 @@
 import { test, expect, request } from '@playwright/test';
 import { ApiUtils } from '../utils/ApiUtils';
 import { StrategyPage } from '../page-objects/strategies-pageObject';
+import { MainNavigationPage } from '../page-objects/mainNavigation-pageObject';
 
 let context: any;
 let page: any;
 let base_url = process.env.BASE_URL;
 let strategyPage: StrategyPage;
+let mainNavigationPage: MainNavigationPage;
 
 test.beforeAll("Setup", async ({browser}) => {
     context = await browser.newContext();
@@ -15,22 +17,37 @@ test.beforeAll("Setup", async ({browser}) => {
     await context.addCookies([{name:"CookieConsent", value: cookieStamp, url: base_url}]);
     page = await context.newPage();
     strategyPage = new StrategyPage(page);
+    mainNavigationPage = new MainNavigationPage(page);
 });
 
-test.describe('Strategy landing pages', () => {
+test.describe('Strategy landing page', () => {
     
-    test.fixme('Can navigate to strategies landing page from mega menu', async () => {
+    test('Can navigate to strategies landing page from mega menu', async () => {
         await context.addCookies([{name:"cookieSelectedRegion", value: "UK", url: base_url}]);
         await context.addCookies([{name:"cookieSelectedChannel", value: "Institutional investor", url: base_url}]);
         await page.goto(base_url + "/en/uk/institutional-investor/");
+        await mainNavigationPage.openStrategyMegaMenu();
+        await mainNavigationPage.clickToViewAllStrategies();
+        await page.waitForURL('**/investment-strategies/');
+        await strategyPage.pageTitleIsCorrect("Investment strategies");
+    });
 
-        // open mega menu
-
-        // click strategies
-
-        // confirm search and ilters are present
-
+    test('Strategy filters and cards are displayed', async () => {
+        await context.addCookies([{name:"cookieSelectedRegion", value: "UK", url: base_url}]);
+        await context.addCookies([{name:"cookieSelectedChannel", value: "Institutional investor", url: base_url}]);
+        await page.goto(base_url + "/en/uk/institutional-investor/investment-strategies/");
+        // confirm search and filters are present
+        await strategyPage.searchBarIsInViewport();
+        await strategyPage.searchBarSearchButtonIsInViewport();
+        await strategyPage.searchFiltersTitleIsInViewport();
+        await strategyPage.searchFiltersListIsInViewport();
+        await strategyPage.multipleSearchFiltersButtonsExist();
         // confirm strategies are listed
+        await strategyPage.multipleStrategyCardsExist();
+    });
+
+    test.fixme('Can apply filters on strategy landing page', async () => {
+
     });
 
     test.fixme('Can open strategy from strategy landing page', async () => {
